3417afa77f0f30c6d02e98b6d3c1dcab27cd3c67
Alexandra Godun <mister.blblbl@gmail.com>
2020-04-11 18:18:23 +0300

    Add basic honk

--- assets/script.js
+++ assets/script.js
@@ -1,35 +1,54 @@
-let x0 = 0, x1 = 0, y0 = 0, y1 = 0, z0 = 0, z1 = 0;
+const permissionBtn = document.getElementById('action-permission');
+const actionBtn = document.getElementById('action-shake');
+const audio = document.getElementById('audio-goose');
 
-function deviceMotionHandler(e) {
-  const sensitivity = 20;
-
-  x0 = e.accelerationIncludingGravity.x;
-  y0 = e.accelerationIncludingGravity.y;
-  z0 = e.accelerationIncludingGravity.z;
-
-  setInterval(function() {
-    var change = Math.abs(x0 - x1 + y0 - y1 + z0 - z1);
-    if (change > sensitivity) {
-      console.log('Shake!');
-    }
-
-    x1 = x0;
-    y1= y0;
-    z1 = z0;
-    color = normalColor;
-  }, 150);
+if (!DeviceMotionEvent.requestPermission) {
+  permissionBtn.style.display = 'none';
+  actionBtn.style.display = 'block';
 }
 
+permissionBtn.addEventListener('click', requestPermissionHahdler);
+actionBtn.addEventListener('click', playAudioHandler);
+
 function requestPermissionHahdler() {
+  audio.play();
+
   DeviceMotionEvent.requestPermission()
     .then(permissionState => {
       if (permissionState === 'granted') {
+        permissionBtn.style.display = 'none';
+        actionBtn.style.display = 'block';
+        
         window.addEventListener('devicemotion', deviceMotionHandler);
       }
     })
     .catch(console.error);
 }
 
+function playAudioHandler() {
+  audio.play();
+}
 
-const button = document.getElementById('shake-action');
-button.addEventListener('click', requestPermissionHahdler);
+let x0 = 0, x1 = 0, y0 = 0, y1 = 0, z0 = 0, z1 = 0;
+
+function deviceMotionHandler(e) {
+  const sensitivity = 15;
+
+  x0 = e.accelerationIncludingGravity.x;
+  y0 = e.accelerationIncludingGravity.y;
+  z0 = e.accelerationIncludingGravity.z;
+
+  setInterval(function() {
+    const change = Math.abs(x0 - x1 + y0 - y1 + z0 - z1);
+
+    if (change > sensitivity) {
+      audio.play();
+    }
+
+    x1 = x0;
+    y1= y0;
+    z1 = z0;
+  }, 150);
+}
+
+
